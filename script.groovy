deaebe887f174d9e69b51fc1aa0829e64ccbe2ee
nme-esridech <70880846+nme-esridech@users.noreply.github.com>
2022-09-03 11:26:50 +0200

    create groovy script

---
+++ script.groovy
@@ -0,0 +1,19 @@
+def buildJar(){
+    echo 'build the application'
+    sh "mvn package"
+}
+
+def buildImage(){
+    echo 'building the docker image'
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+        sh 'docker build -t nme4esri/my-repo:jma-2.0 .'
+        sh 'echo $PASSWORD | docker login -u $USERNAME --password-stdin'
+        sh 'docker push nme4esri/my-repo:jma-2.0'
+    }
+}
+
+def deployApp(){
+    echo 'deploy the application'
+}
+
+return this
